ef3a7d6dc14872e0c57abfe2d779763b38d952db
Cesar Velasquez <cesar.velasquez06@gmail.com>
2017-07-31 15:44:52 -0400

    Add personal gps services.

--- app/src/main/java/cl/underline/gps_tracker/Activity/MainActivity.java
+++ app/src/main/java/cl/underline/gps_tracker/Activity/MainActivity.java
@@ -1,4 +1,4 @@
-package cl.underline.gps_tracker;
+package cl.underline.gps_tracker.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(cl.underline.gps_tracker.R.layout.activity_main);
     }
 }
